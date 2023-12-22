51b754e018b70741d053919759a1c73c4a40de3e
Yishak <yishakdemsew@gmail.com>
2023-12-22 13:04:22 +0300

    added backend url from render.com

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://127.0.0.1:5001/byizak-915e7/us-central1/api",
+    baseURL: "https://amazon-backend-cxlb.onrender.com",
     // "http://localhost:5001/clone-d613f/us-central1/api", // the api url (cloud function url)
 });
 
